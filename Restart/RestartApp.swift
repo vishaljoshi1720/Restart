3dceff34d0c4824e759eee58ea0b4219d8fbac9f
Vishal Joshi <vishaljoshi2215889@gmail.com>
2021-12-07 21:59:39 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Vishal Joshi on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
